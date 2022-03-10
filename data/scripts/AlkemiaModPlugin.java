9c8be9ef9aa5af578d8abb9e530c048c4bb7a00c
Alfred Broda <alfred.broda@gmail.com>
2022-03-10 10:03:44 +0100

    Basic assets and code

---
+++ data/scripts/AlkemiaModPlugin.java
@@ -0,0 +1,88 @@
+package data.scripts;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.ModSpecAPI;
+import com.fs.starfarer.api.PluginPick;
+import com.fs.starfarer.api.campaign.CampaignPlugin;
+import com.fs.starfarer.api.combat.AutofireAIPlugin;
+import com.fs.starfarer.api.combat.MissileAIPlugin;
+import com.fs.starfarer.api.combat.MissileAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.WeaponAPI;
+import com.fs.starfarer.api.impl.campaign.intel.bar.events.BarEventManager;
+import com.fs.starfarer.api.impl.campaign.shared.SharedData;
+import com.thoughtworks.xstream.XStream;
+import exerelin.campaign.SectorManager;
+import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.dark.shaders.light.LightData;
+import org.dark.shaders.util.ShaderLib;
+import org.dark.shaders.util.TextureData;
+
+public class AlkemiaModPlugin extends BaseModPlugin
+{
+    public static final String SETTINGS_FILE = "alkemia_options.ini";
+
+    public static final boolean isExerelin = Global.getSettings().getModManager().isModEnabled("nexerelin");
+    public static boolean versionCheckerNag;
+    public static boolean vcNagDone = false;
+
+    @Override
+    public void onApplicationLoad()
+    {
+        boolean hasLazyLib = Global.getSettings().getModManager().isModEnabled("lw_lazylib");
+        if (!hasLazyLib)
+        {
+            throw new RuntimeException("Alkemia Armoury requires LazyLib!"
+                    + "\nGet it at http://fractalsoftworks.com/forum/index.php?topic=5444");
+        }
+
+        boolean hasMagicLib = Global.getSettings().getModManager().isModEnabled("MagicLib");
+        if (!hasMagicLib)
+        {
+            throw new RuntimeException("Alkemia Armoury requires MagicLib!"
+                    + "\nGet it at http://fractalsoftworks.com/forum/index.php?topic=13718");
+        }
+
+        // boolean hasShaderLib = Global.getSettings().getModManager().isModEnabled("shaderLib");
+        // if (!hasShaderLib)
+        // {
+        //     throw new RuntimeException("Alkemia Armoury requires GraphicsLib!"
+        //             + "\nGet it at http://fractalsoftworks.com/forum/index.php?topic=10982");
+        // }
+        // else
+        // {
+        //     initShaderLib();
+        // }
+
+    }
+
+    // static void initShaderLib()
+    // {
+    //     ShaderLib.init();
+        // LightData.readLightDataCSV("data/lights/tiandong_light_data.csv");
+        // TextureData.readTextureDataCSV("data/lights/tiandong_texture_data.csv");
+    // }
+
+    @Override
+    public void onGameLoad(boolean newGame)
+    {
+        Global.getLogger(this.getClass()).info("On game load");
+    }
+
+    @Override
+    public void onNewGame()
+    {
+        Global.getLogger(this.getClass()).info("On new game");
+        // Global.getSector().registerPlugin(new TiandongCampaignPlugin());
+        // tiandong_ModPluginAlt.initTiandong();
+        // SharedData.getData().getPersonBountyEventData().addParticipatingFaction("tiandong");
+    }
+
+    @Override
+    public void onNewGameAfterEconomyLoad()
+    {
+    }
+}
---
+++ data/weapons/hullmods/AlkemiaImprovedManufactory.java
@@ -0,0 +1,47 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
+
+public class DefectiveManufactory extends BaseHullMod {
+
+	public static float SPEED_IMPROVEMENT = 0.10f;
+	public static float DAMAGE_DECREASE = -0.10f;
+	
+	
+	public void applyEffectsToFighterSpawnedByShip(ShipAPI fighter, ShipAPI ship, String id) {
+		float effect = ship.getMutableStats().getDynamic().getValue(Stats.DMOD_EFFECT_MULT);
+		
+		MutableShipStatsAPI stats = fighter.getMutableStats();
+		
+		stats.getMaxSpeed().modifyMult(id, 1f + SPEED_IMPROVEMENT * effect);
+
+		stats.getArmorDamageTakenMult().modifyPercent(id, DAMAGE_DECREASE * 100f * effect);
+		stats.getShieldDamageTakenMult().modifyPercent(id, DAMAGE_DECREASE * 100f * effect);
+		stats.getHullDamageTakenMult().modifyPercent(id, DAMAGE_DECREASE * 100f * effect);
+
+		//fighter.setHeavyDHullOverlay();
+		//fighter.setLightDHullOverlay();
+	}
+	
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		CompromisedStructure.modifyCost(hullSize, stats, id);
+	}
+	
+		
+	public String getDescriptionParam(int index, HullSize hullSize, ShipAPI ship) {
+		float effect = 1f;
+		if (ship != null) effect = ship.getMutableStats().getDynamic().getValue(Stats.DMOD_EFFECT_MULT);
+		
+		if (index == 0) return "" + (int) Math.round(SPEED_IMPROVEMENT * 100f * effect) + "%";
+		if (index == 1) return "" + (int) Math.round(DAMAGE_DECREASE * 100f * effect) + "%";
+		return null;
+	}
+}
+
+
+
+
