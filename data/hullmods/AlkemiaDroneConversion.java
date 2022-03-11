bd1671093ddfd3031e39a7ab673f1212f448e3de
Alfred Broda <alfred.broda@gmail.com>
2022-03-11 23:59:50 +0100

    Fixed returning wings to cargo, add 1 bay only

--- data/hullmods/AlkemiaDroneConversion.java
+++ data/hullmods/AlkemiaDroneConversion.java
@@ -29,8 +29,8 @@ import com.fs.starfarer.api.campaign.FleetDataAPI;
 public class AlkemiaDroneConversion extends BaseHullMod {
 
 	public static final int CREW_REQ = -10;
-	public static final int ALL_DRONE_COST_REDUCTION = -50;
-	public static final int BAY_SPACE_GAIN_PERCENT = 40;
+	public static final int ALL_DRONE_COST_REDUCTION = -20;
+	public static final int BAY_SPACE_GAIN = 1;
 	public static final int REARM_SPEEDUP_PERCENT = -20;
 
 	protected Logger log;
@@ -45,7 +45,7 @@ public class AlkemiaDroneConversion extends BaseHullMod {
 
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
 		stats.getFighterRefitTimeMult().modifyPercent(id, REARM_SPEEDUP_PERCENT);
-		stats.getNumFighterBays().modifyPercent(id, BAY_SPACE_GAIN_PERCENT);
+		stats.getNumFighterBays().modifyFlat(id, BAY_SPACE_GAIN, "Expanded bays by drone conversion.");
 
 		stats.getMinCrewMod().modifyFlat(id, CREW_REQ * stats.getNumFighterBays().getBaseValue());
 
@@ -59,34 +59,36 @@ public class AlkemiaDroneConversion extends BaseHullMod {
 		// source is avaliable)
 		ShipVariantAPI variant = stats.getVariant();
 		List<String> wings = variant.getWings();
-		removedWings.clear();
 		for (int i = 0; i < wings.size(); i++) {
 			FighterWingSpecAPI currentWing = variant.getWing(i);
 			if (currentWing != null && !currentWing.hasTag(Tags.AUTOMATED_FIGHTER)) {
 				variant.setWingId(i, null);
 				removedWings.add(currentWing.getId());
-				// this.addToCargo(ship, wing);
 			}
 		}
 
-		// listeners = stats.getListeners() maybe there is one for when wings are
-		// picked?
 		if (removedWings != null && !removedWings.isEmpty()) {
-			log.warn("Wings removed missing from cargo:");
+			log.warn("Wings removed:");
 			for (String wing : removedWings) {
-				log.warn(wing);
+				log.warn(" - " + wing);
 			}
-
 		}
+		// stats.getListeners() maybe there is one for when wings are picked?
 	}
 
-	public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
-		// ship.getVariant().addMod(HullMods.AUTOMATED);
+	public void advanceInCampaign(FleetMemberAPI member, float amount) {
 		if (removedWings != null && !removedWings.isEmpty()) {
-			for (String wing : removedWings) {
-				this.addToCargo(ship, wing);
+			log.info("Wings to return:");
+			CargoAPI cargo = member.getFleetData().getFleet().getCargo();
+			if (cargo != null) {
+				for (String wing : removedWings) {
+					cargo.addFighters(wing, 1);
+					log.info(" - " + wing);
+				}
+				removedWings.clear();
+			} else {
+				log.error("advanceInCampaign: Cannot get CargoAPI for " + member.getHullId());
 			}
-
 		}
 	}
 
@@ -94,31 +96,13 @@ public class AlkemiaDroneConversion extends BaseHullMod {
 		cargo.addFighters(wing, 1);
 	}
 
-	private void addToCargo(ShipAPI ship, String wing) {
-		FleetMemberAPI member = ship.getFleetMember(); // returns null :/
-		if (member != null) {
-			CargoAPI cargo = member.getFleetData().getFleet().getCargo();
-			if (cargo != null) {
-				cargo.addFighters(wing, 1);
-			} else {
-				log.error("applyEffectsAfterShipCreation: Cannot get CargoAPI for " + ship.getId() + " "
-						+ ship.getHullSpec().getHullId());
-			}
-		} else {
-			log.error("applyEffectsAfterShipCreation: Cannot get FleetMemberAPI for " + ship.getId() + " "
-					+ ship.getHullSpec().getHullId());
-		}
-	}
-
 	public boolean isApplicableToShip(ShipAPI ship) {
-		return ship != null && !ship.isFrigate() &&
+		return ship != null &&
 				ship.getNumFighterBays() > 0 &&
 				!ship.getVariant().hasHullMod(HullMods.PHASE_FIELD);
 	}
 
 	public String getUnapplicableReason(ShipAPI ship) {
-		if (ship != null && ship.isFrigate())
-			return "Can not be installed on a frigate";
 		if (ship != null && ship.getVariant().hasHullMod(HullMods.PHASE_FIELD))
 			return "Can not be installed on a phase ship";
 
@@ -133,7 +117,7 @@ public class AlkemiaDroneConversion extends BaseHullMod {
 		if (index == 0)
 			return "" + CREW_REQ;
 		if (index == 1)
-			return "" + BAY_SPACE_GAIN_PERCENT + "%";
+			return "" + BAY_SPACE_GAIN;
 		if (index == 2)
 			return "" + REARM_SPEEDUP_PERCENT + "%";
 
