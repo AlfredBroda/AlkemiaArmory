986f54bf30165157f7d79187779a629e556307b6
Alfred Broda <alfred.broda@gmail.com>
2022-10-27 00:40:36 +0200

    Redesigned orbitals

--- src/data/hullmods/AlkemiaRimeRepaints.java
+++ src/data/hullmods/AlkemiaRimeRepaints.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Logger;
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
-import com.fs.starfarer.api.combat.ShipHullSpecAPI;
 import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.Global;
